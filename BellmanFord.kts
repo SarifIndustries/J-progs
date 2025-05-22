4fce5be89049c134ad2b6cb04bc0834d63dd3710
Sarif Industries <severoffdm@gmail.com>
2025-05-22 17:47:17 +0500

    Add NegativeCycleCheck

--- BellmanFord.kts
+++ BellmanFord.kts
@@ -1,8 +1,8 @@
 val inf = 9000
 val n = 5
-val u = intArrayOf(2, 1, 1, 4, 3).map { it-1 }
-val v = intArrayOf(3, 2, 5, 5, 4).map { it-1 }
-val w = intArrayOf(2, -3, 5, 2, 3)
+val u = intArrayOf(2, 1, 5, 4, 3).map { it-1 }
+val v = intArrayOf(3, 2, 1, 5, 4).map { it-1 }
+val w = intArrayOf(2, -3, 5, -100, 3)
 val dis: IntArray = IntArray(n) { if(it == 0) 0 else inf }
 repeat(n) {
     for(i in 0..<u.size) {
@@ -13,3 +13,13 @@ repeat(n) {
     }
     println(dis.joinToString())
 }
+// Negative cycle check
+var flag = false
+for(i in 0..<u.size) {
+    val source = u[i]
+    val target = v[i]
+    val shortcut = dis[source] + w[i]
+    println("Shortcut: $shortcut    Dist: ${dis[target]}")
+    if (shortcut < dis[target]) { flag = true; break }
+}
+println("Negative cycles: ${if(flag) "detected" else "none"}")
